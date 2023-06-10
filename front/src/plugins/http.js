9f7fa7317ead3a52dda3b38273c1d52e60aeab18
hope <nadezdaturbina00511@gmail.com>
2023-06-10 18:36:16 +0300

    исправления докера

--- front/src/plugins/http.js
+++ front/src/plugins/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
     install(Vue, options) {
         Vue.prototype.$http = axios.create({
-            baseUrl: "http://host.docker.internal:8181",
+            baseUrl: "http://localhost:8181",
             headers: options.headers || null
         })
     }
